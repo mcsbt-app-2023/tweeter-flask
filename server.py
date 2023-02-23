f9fd991a080b9fb249419b5797e543abb43048ce
Pepe García <pepe@pepegar.com>
2023-02-23 22:20:07 +0100

    prepare for class

--- server.py
+++ server.py
@@ -8,143 +8,51 @@ engine = create_engine("sqlite:///tweeter.db")
 
 @tweeter.route("/")
 def index():
-    tweets = []
-    if "username" in session:
-        query = f"""
-        SELECT u.id, u.picture, u.username, t.tweet
-        FROM tweets t
-        INNER JOIN users u ON t.user_id=u.id
-        INNER JOIN follows f ON f.followee_id=u.id
-        WHERE f.follower_id={session['user_id']}
-        """
-
-        with engine.connect() as connection:
-            tweets = connection.execute(query).fetchall()
-
-    return render_template("index.html", tweets=tweets)
+    return render_template("index.html")
 
 @tweeter.route("/register")
 def register():
-    return render_template("register.html")
+    pass
 
 @tweeter.route("/register", methods=["POST"])
 def handle_register():
-    username=request.form["username"]
-    password=request.form["password"]
-    picture=request.form["picture"]
-
-    hashed_password = generate_password_hash(password)
-
-    insert_query = f"""
-    INSERT INTO users(username, picture, password)
-    VALUES ('{username}', '{picture}', '{hashed_password}')
-    """
-
-    with engine.connect() as connection:
-        connection.execute(insert_query)
-
-        return redirect(url_for("index"))
-
+    pass
 
 @tweeter.route("/users")
 def users():
-    query = """
-    SELECT id, username, picture
-    FROM users
-    """
-
-    with engine.connect() as connection:
-        users = connection.execute(query)
-
-        return render_template("users.html", users=users)
-
+    pass
 
 @tweeter.route("/users/<user_id>")
 def user_detail(user_id):
-    query = f"""
-    SELECT id, username, picture
-    FROM users
-    where id={user_id}
-    """
-
-    tweets_query = f"""
-    SELECT tweet
-    FROM tweets
-    WHERE user_id={user_id}
-    """
-
-    with engine.connect() as connection:
-        user = connection.execute(query).fetchone()
-        tweets = connection.execute(tweets_query).fetchall()
-
-        if user:
-            return render_template("user_detail.html", user=user, tweets=tweets)
-        else:
-            return render_template("404.html"), 404
+    pass
 
 @tweeter.route("/login")
 def login():
-    return render_template("login.html")
+    pass
 
 @tweeter.route("/login", methods=["POST"])
 def handle_login():
-    username=request.form["username"]
-    password=request.form["password"]
-
-    login_query = f"""
-    SELECT password, id
-    FROM users
-    WHERE username='{username}'
-    """
-
-    with engine.connect() as connection:
-        user = connection.execute(login_query).fetchone()
-
-        if user and check_password_hash(user[0], password):
-            session["user_id"] = user[1]
-            session["username"] = username
-            return redirect(url_for("index"))
-        else:
-            return render_template("404.html"), 404
+    pass
 
 @tweeter.route("/logout")
 def logout():
-    session.pop("username")
-    session.pop("user_id")
-
-    return redirect(url_for("index"))
+    pass
 
 
 @tweeter.route("/tweet", methods=["POST"])
 def handle_tweet():
-    tweet=request.form["tweet"]
-
-    insert_query = f"""
-    INSERT INTO tweets(tweet, user_id)
-    VALUES ('{tweet}', {session['user_id']})
-    """
-
-    with engine.connect() as connection:
-        connection.execute(insert_query)
-
-        return redirect(url_for("index"))
+    pass
 
 @tweeter.route("/follow/<followee>")
 def follow(followee):
-    if "user_id" not in session:
-        return render_template("403.html"), 403    
-    else:
-        follower = session["user_id"]
+    pass
 
-        insert_query = f"""
-        INSERT INTO follows(follower_id, followee_id)
-        VALUES ({follower}, {followee})
-        """
+@tweeter.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-        with engine.connect() as connection:
-            connection.execute(insert_query)
-
-            return redirect(url_for("index"))
-
+@tweeter.errorhandler(403)
+def unauthorized(e):
+    return render_template('403.html'), 403
 
 tweeter.run(debug=True, port=8080)
